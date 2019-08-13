bd00d54879c8bfd2a54e1bb90a6e5c24f2330e38
Mathijs Genuit <MGenuit@sysqa.nl>
2019-08-13 17:14:28 +0200

    Verplaats voorbeeld naar eigen project
    Zo blijft het originele project mooi schoon voor gebruik.

---
+++ SYSQA.Resource.CSharp.Selenium/SYSQA.Resource.CSharp.Selenium/PageObjects/SUT/ExampleApp.cs
@@ -0,0 +1,14 @@
+﻿using SYSQA.Resource.CSharp.Selenium.PageObjects.Template;
+using SYSQA.Resource.CSharp.Selenium.Utilities;
+
+namespace SYSQA.Resource.CSharp.Selenium.PageObjects.SUT
+{
+    public class ExampleApp
+    {
+        public TemplatePage GoToHomePage()
+        {
+            BrowserManager.Instance.Navigate().GoToUrl("https://www.sysqa.nl/");
+            return new TemplatePage();
+        }
+    }
+}
